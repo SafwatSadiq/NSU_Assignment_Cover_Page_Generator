da6ebd9709712563b03e31e0d6f7c8bd1a2800b0
Safwat Sadiq <Safwatsadiq14@gmail.com>
2025-07-16 20:56:27 +0600

    Added A New Function Named add_text to make the code smaller

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ Id = input("Enter your id: ").strip().title()
 Sec = input("Enter your sec: ").strip().title()
 Date = input("Enter your date: ").strip().title()
 
-def draw_border(FPDF, x):
+def draw_border(pdf, x):
     """ Draws a border around the page with a specified margin."""
     pdf.set_draw_color(34, 126, 172)
     pdf.set_line_width(0.3)
     pdf.rect(x=x, y=x, w=pdf.w - 2 * x, h=pdf.h - 2 * x)
+    
+def add_text(pdf, x_1, y_1, x_2, y_2, text_1, text_2, font_size=20):
+    pdf.set_font("Nimbus_Rom_No_9L", style="B", size=font_size)
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_xy(x_1, y_1)
+    pdf.cell(0, 20, text=text_1, new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
+    pdf.set_font("Nimbus_Rom_No_9L", size=20)
+    pdf.set_xy(x_2, y_2)
+    pdf.cell(0, 20, text=text_2, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
+    
     
 # Page Setup
 pdf = FPDF(format='A4') 
@@ -41,6 +51,7 @@ pdf.cell(pdf.w + 2, 50, text=Header, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align=
 pdf.set_font("helvetica", style="B",  size=20)
 pdf.set_text_color(255, 255, 255)
 pdf.set_fill_color(34, 126, 172)
+pdf.set_draw_color(0, 0, 0)
 pdf.rect(x=pdf.w/2 - pdf.get_string_width(Course) / 2, y=114.5,
          w=pdf.get_string_width(Course) + 2.2, h=10,
          round_corners=True, corner_radius=10, style="DF")
@@ -48,49 +59,15 @@ pdf.set_xy(pdf.w/2 - pdf.get_string_width(Course) / 2, 115)
 pdf.cell(pdf.get_string_width(Course) + 2, 10, text=Course, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
 
 # Submitted To Part
-pdf.set_font("Nimbus_Rom_No_9L", style="B", size=20)
-pdf.set_text_color(0, 0, 0)
-pdf.set_xy(20, 140)
-pdf.cell(0, 20, text="Submitted To: ", new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
-pdf.set_font("Nimbus_Rom_No_9L", size=20)
-pdf.set_xy(65, 140)
-pdf.cell(0, 20, text=Faculty, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
-
+add_text(pdf, 20, 140, 65, 140, "Submitted To: ", Faculty)
 # Submitted By Part
-pdf.set_font("Nimbus_Rom_No_9L", style="B", size=20)
-pdf.set_text_color(0, 0, 0)
-pdf.set_xy(20, 180)
-pdf.cell(0, 20, text="Submitted By: ", new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
-pdf.set_font("Nimbus_Rom_No_9L", size=20)
-pdf.set_xy(65, 180)
-pdf.cell(0, 20, text=Name, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
-
+add_text(pdf, 20, 180, 65, 180, "Submitted By: ", Name)
 # ID Part
-pdf.set_font("Nimbus_Rom_No_9L", style="B", size=20)
-pdf.set_text_color(0, 0, 0)
-pdf.set_xy(20, 190)
-pdf.cell(0, 20, text="ID: ", new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
-pdf.set_font("Nimbus_Rom_No_9L", size=20)
-pdf.set_xy(32, 190)
-pdf.cell(0, 20, text=Id, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
-
+add_text(pdf, 20, 190, 32, 190, "ID: ", Id)
 # Sec Part
-pdf.set_font("Nimbus_Rom_No_9L", style="B", size=20)
-pdf.set_text_color(0, 0, 0)
-pdf.set_xy(20, 200)
-pdf.cell(0, 20, text="Sec: ", new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
-pdf.set_font("Nimbus_Rom_No_9L", size=20)
-pdf.set_xy(34, 200)
-pdf.cell(0, 20, text=Sec, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
-
+add_text(pdf, 20, 200, 34, 200, "Sec: ", Sec)
 # Date Part
-pdf.set_font("Nimbus_Rom_No_9L", style="B", size=20)
-pdf.set_text_color(0, 0, 0)
-pdf.set_xy(20, 210)
-pdf.cell(0, 20, text="Date: ", new_x=XPos.RIGHT, new_y=YPos.TOP, align='L')
-pdf.set_font("Nimbus_Rom_No_9L", size=20)
-pdf.set_xy(38, 210)
-pdf.cell(0, 20, text=Date, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='L')
+add_text(pdf, 20, 210, 38, 210, "Date: ", Date)
 
 
 pdf.output("Output\\output.pdf")
