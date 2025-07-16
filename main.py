5497d24e8a73753d65275320cdfc4f010c68491d
Safwat Sadiq <Safwatsadiq14@gmail.com>
2025-07-16 21:36:14 +0600

    Minor changes of slashes

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ def generate_pdf():
     draw_border(pdf, 6.7)
 
     # Adding New Fonts
-    pdf.add_font("Bay_Tavern", "", "Fonts\\Bay_Tavern\\Bay Tavern S Regular.ttf")
-    pdf.add_font("Nimbus_Rom_No_9L", "B", "Fonts\\Nimbus_Rom_No_9L\\NimbusRomNo9L-Med.ttf")
-    pdf.add_font("Nimbus_Rom_No_9L", "", "Fonts\\Nimbus_Rom_No_9L\\NimbusRomNo9L-Reg.ttf")
+    pdf.add_font("Bay_Tavern", "", "Fonts/Bay_Tavern/Bay Tavern S Regular.ttf")
+    pdf.add_font("Nimbus_Rom_No_9L", "B", "Fonts/Nimbus_Rom_No_9L/NimbusRomNo9L-Med.ttf")
+    pdf.add_font("Nimbus_Rom_No_9L", "", "Fonts/Nimbus_Rom_No_9L/NimbusRomNo9L-Reg.ttf")
 
     # Working With The NSU Logo
     page_center = pdf.w / 2
